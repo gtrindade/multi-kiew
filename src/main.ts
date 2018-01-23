3e5815836a5f4cf776da092dce6f633d9a583372
Guilherme Goncalves <ggoncalves@fanatics.com>
2018-01-22 18:17:53 -0800

    adding (AF)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as Slimbot from 'slimbot'
 import * as Roll from 'roll'
-// import * as fs from 'fs'
 import * as net from 'net'
 
 const roll = new Roll()
@@ -14,22 +13,27 @@ const ASSUNTOS = [`A terra é redonda!`, `PC > VG`, `Star Wars I, II, III é bom
 const MINE_HOST = `73.15.19.24`
 const MINE_PORT = 25565
 const TIMEOUT = 3000
+const aishoUrl = `https://imgur.com/oMTaDat`
 
 // Register listeners
 slimbot.on(`message`, (message: any) => {
-  const {text} = message
+  const {text, chat} = message
   if (text) {
+    if (text.indexOf(`(AF)`) >= 0) {
+      slimbot.sendPhoto(chat.id, aishoUrl)
+        .catch(console.log)
+    }
     if (text.startsWith(ROLL)) {
       var expression = text.substring(ROLL.length, text.length).trim()
       const [rollExpression] = expression.split(` `)
 
       if (rollExpression.length > 7) {
-        slimbot.sendMessage(message.chat.id, message.from.username + `, sério mesmo... sem trollar.`)
+        slimbot.sendMessage(chat.id, message.from.username + `, sério mesmo... sem trollar.`)
         return
       }
 
       var output = roll.roll(rollExpression)
-      slimbot.sendMessage(message.chat.id, message.from.username + ` => ` + expression + `:\n\nRolled: [ ` + output.rolled + ` ]\nTotal: ` + output.result)
+      slimbot.sendMessage(chat.id, message.from.username + ` => ` + expression + `:\n\nRolled: [ ` + output.rolled + ` ]\nTotal: ` + output.result)
     } else if (text.startsWith(SERVER_STATUS)) {
       var target = text.substring(SERVER_STATUS.length, text.length).trim()
 
@@ -41,24 +45,23 @@ slimbot.on(`message`, (message: any) => {
         client.setTimeout(TIMEOUT)
 
         client.connect(MINE_PORT, MINE_HOST, () => {
-          slimbot.sendMessage(message.chat.id, msgPrefix + `:\n\nEstá ligado! =D`)
+          slimbot.sendMessage(chat.id, msgPrefix + `:\n\nEstá ligado! =D`)
           client.destroy()
         })
         const errorHandler = () => {
-          slimbot.sendMessage(message.chat.id, msgPrefix + `:\n\nEstá desligado... =(`)
+          slimbot.sendMessage(chat.id, msgPrefix + `:\n\nEstá desligado... =(`)
           client.destroy()
         }
         client.on(`timeout`, errorHandler)
         client.on(`error`, errorHandler)
 
-
         break
       default:
-        slimbot.sendMessage(message.chat.id, SERVER_STATUS.substring(1, SERVER_STATUS.length) + ` => ` + target + `:\n\nIsso non ecziste!`)
+        slimbot.sendMessage(chat.id, SERVER_STATUS.substring(1, SERVER_STATUS.length) + ` => ` + target + `:\n\nIsso non ecziste!`)
         break
       }
     } else if (text.startsWith(POLEMICA)) {
-      slimbot.sendMessage(message.chat.id, `Blz, lá vai:\n\n` + ASSUNTOS[Math.floor(Math.random() * ASSUNTOS.length)])
+      slimbot.sendMessage(chat.id, `Blz, lá vai:\n\n` + ASSUNTOS[Math.floor(Math.random() * ASSUNTOS.length)])
     }
   }
 })
